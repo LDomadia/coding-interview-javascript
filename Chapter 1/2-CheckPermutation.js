a04c3bf16cecbb4837d9cd238253f85fd673e738
Leena Domadia <leena.domadia2018@gmail.com>
2022-07-02 15:39:25 -0400

    prettier

--- Chapter 1/2-CheckPermutation.js
+++ Chapter 1/2-CheckPermutation.js
@@ -1,29 +1,27 @@
 function checkPermutation(s1, s2) {
-    if (s1.length != s2.length) {
-        return false;
+  if (s1.length != s2.length) {
+    return false;
+  }
+  var charObj = {};
+  for (let i = 0; i < s1.length; i++) {
+    if (!charObj[s1.charAt(i)]) {
+      charObj[s1.charAt(i)] = 1;
+    } else {
+      charObj[s1.charAt(i)]++;
     }
-    var charObj = {}
-    for (let i = 0; i < s1.length; i++) {
-        if (!charObj[s1.charAt(i)]) {
-            charObj[s1.charAt(i)] = 1;
-        }
-        else {
-            charObj[s1.charAt(i)]++;
-        }
-    }   
+  }
 
-    for (let i = 0; i < s2.length; i++) {
-        if (!charObj[s2.charAt(i)]) {
-            return false;
-        }
-        else {
-            charObj[s2.charAt(i)]--;
-            if (charObj[s2.charAt(i)] < 0) {
-                return false;
-            }
-        }
+  for (let i = 0; i < s2.length; i++) {
+    if (!charObj[s2.charAt(i)]) {
+      return false;
+    } else {
+      charObj[s2.charAt(i)]--;
+      if (charObj[s2.charAt(i)] < 0) {
+        return false;
+      }
     }
-    return true;
+  }
+  return true;
 }
 
 console.log(checkPermutation("abcd", "abcd"));
